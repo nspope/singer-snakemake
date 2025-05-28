cac70e784612e8e36290ecf47aeb489bef76192b
Nate Pope <nspope@utexas.edu>
2025-05-28 10:01:37 -0700

    Replace coalescence rate diagnostics with coalescence pdf

--- workflow/scripts/coalescence_rates.py
+++ workflow/scripts/coalescence_rates.py
@@ -7,7 +7,6 @@ Part of https://github.com/nspope/singer-snakemake.
 
 import sys
 import pickle
-import numba
 import msprime
 import numpy as np
 import tskit
@@ -20,256 +19,43 @@ def tag():
     return f"[singer-snakemake::{snakemake.rule}::{str(datetime.now())}]"
 
 
-@numba.njit("f8[:, :, :](i4[:, :], f8[:], f8[:, :], i4[:], i4[:], f8[:], f8[:], i4[:], i4[:], f8)")
-def _pair_coalescence_counts(
-    indexes, # (num_indexes, 2)
-    windows, # (num_windows + 1)
-    nodes_sample, # (num_nodes, len(sample_sets))
-    edges_child,
-    edges_parent,
-    edges_left,
-    edges_right,
-    insert_index,
-    remove_index,
-    sequence_length,
+def weighted_pair_coalescence_rates(
+    ts, sample_sets, indexes, windows, window_weights, 
+    num_time_bins=25, log_time_bounds=[1, 7],
 ):
     """
-    Calculate pair coalescence rates in windows across the genome, within
-    and between sample sets.
-
-    This is a bastardization of `ts.pair_coalescence_counts` for numba:
-
-    https://github.com/tskit-dev/tskit/issues/2904
-
-    and will eventually be removed in favour of the native tskit method.
+    Calculate marginal pair coalescence rates, weighted by the proportion of
+    missing data in each window and summed over windows
     """
-
-    assert indexes.max() < nodes_sample.shape[1]
-    assert edges_child.size == edges_parent.size == edges_left.size == edges_right.size
-    assert edges_child.size == insert_index.size == remove_index.size
-    assert windows.size > 1
-
-    num_nodes = nodes_sample.shape[0]
-    num_edges = edges_child.size
-    num_windows = windows.size - 1
-    num_indexes = indexes.shape[0]
-
-    insert_position = edges_left[insert_index]
-    remove_position = edges_right[remove_index]
-
-    nodes_parent = np.full(num_nodes, tskit.NULL)
-    coalescing_pairs = np.zeros((num_windows, num_nodes, num_indexes))
-    sample_counts = nodes_sample.copy()
-
-    position = 0.0
-    w, a, b = 0, 0, 0
-    while position < sequence_length:
-        remainder = sequence_length - position
-
-        while b < num_edges and remove_position[b] == position:  # edges out
-            e = remove_index[b]
-            p = edges_parent[e]
-            c = edges_child[e]
-            nodes_parent[c] = tskit.NULL
-            inside = sample_counts[c]
-            while p != tskit.NULL:
-                outside = sample_counts[p] - sample_counts[c] - nodes_sample[p]
-                for i, (j, k) in enumerate(indexes):
-                    weight = inside[j] * outside[k] + inside[k] * outside[j]
-                    coalescing_pairs[w, p, i] -= weight * remainder
-                c, p = p, nodes_parent[p]
-            p = edges_parent[e]
-            while p != tskit.NULL:
-                sample_counts[p] -= inside
-                p = nodes_parent[p]
-            b += 1
-
-        while a < num_edges and insert_position[a] == position:  # edges in
-            e = insert_index[a]
-            p = edges_parent[e]
-            c = edges_child[e]
-            nodes_parent[c] = p
-            inside = sample_counts[c]
-            while p != tskit.NULL:
-                sample_counts[p] += inside
-                p = nodes_parent[p]
-            p = edges_parent[e]
-            while p != tskit.NULL:
-                outside = sample_counts[p] - sample_counts[c] - nodes_sample[p]
-                for i, (j, k) in enumerate(indexes):
-                    weight = inside[j] * outside[k] + inside[k] * outside[j]
-                    coalescing_pairs[w, p, i] += weight * remainder
-                c, p = p, nodes_parent[p]
-            a += 1
-
-        position = sequence_length
-        if b < num_edges:
-            position = min(position, remove_position[b])
-        if a < num_edges:
-            position = min(position, insert_position[a])
-
-        while w < num_windows and windows[w + 1] <= position:  # flush window
-            remainder = sequence_length - windows[w + 1]
-            for c, p in enumerate(nodes_parent):
-                if p == tskit.NULL:
-                    continue
-                inside = sample_counts[c]
-                outside = sample_counts[p] - sample_counts[c] - nodes_sample[p]
-                for i, (j, k) in enumerate(indexes):
-                    weight = inside[j] * outside[k] + inside[k] * outside[j]
-                    coalescing_pairs[w, p, i] -= weight * remainder / 2
-                    if w + 1 < num_windows:
-                        coalescing_pairs[w + 1, p, i] += weight * remainder / 2
-            w += 1
-
-    for i, (j, k) in enumerate(indexes):
-        if j == k:
-            coalescing_pairs[:, :, i] /= 2
-
-    for w, s in enumerate(windows[1:] - windows[:-1]):
-        coalescing_pairs[w] /= s
-
-    return coalescing_pairs
-
-
-# TODO: invoke this inside numba above, 
-def _rates_from_ecdf(weights, atoms, quantiles):
-    assert weights.size == atoms.size
-    assert np.all(weights > 0.0)
-    assert np.all(atoms > 0.0)
-    assert np.all(np.diff(atoms) >= 0.0)
-    assert quantiles[0] == 0.0 and quantiles[-1] == 1.0
-    assert quantiles.size > 2
-
-    # find interior quantiles
-    weights = np.append(0, weights)
-    atoms = np.append(0, atoms)
-    ecdf = np.cumsum(weights)
-    indices = np.searchsorted(ecdf, ecdf[-1] * quantiles[1:-1], side='left')
-    lower, upper = atoms[indices - 1], atoms[indices]
-    ecdfl, ecdfu = ecdf[indices - 1] / ecdf[-1], ecdf[indices] / ecdf[-1]
-    assert np.all(np.logical_and(quantiles[1:-1] > ecdfl,  quantiles[1:-1] < ecdfu))
-
-    # interpolate ECDF
-    assert np.all(ecdfu - ecdfl > 0)
-    slope = (upper - lower) / (ecdfu - ecdfl)
-    breaks = np.append(0, lower + slope * (quantiles[1:-1] - ecdfl))
-
-    # calculate coalescence rates within intervals
-    # this uses a Kaplan-Meier-type censored estimator: https://github.com/tskit-dev/tskit/pull/2119
-    coalrate = np.full(quantiles.size - 1, np.nan)
-    propcoal = np.diff(quantiles[:-1]) / (1 - quantiles[:-2])
-    coalrate[:-1] = -np.log(1 - propcoal) / np.diff(breaks)
-    last = indices[-1]
-    coalrate[-1] = np.sum(weights[last:]) / np.dot(atoms[last:] - breaks[-1], weights[last:]) 
-
-    return coalrate, breaks
-
-def weighted_pair_coalescence_rates(ts, sample_sets, indexes, windows, window_weights, num_time_bins=50):
-    """
-    Calculate pair coalescence rates within equally-spaced quantiles over the
-    chromosome, with a weighted contribution over genomic windows (ie weighted
-    by missing data).  E.g. calculate the inverse of effective population size
-    in N epochs wherein a proportion 1/N of pairs coalescece.
-    """
-
-    nodes_sample = np.zeros((ts.num_nodes, len(sample_sets)))
-    for i, s in enumerate(sample_sets):
-        nodes_sample[s, i] = 1
-
-    indexes_array = np.zeros((len(indexes), 2), dtype=np.int32)
-    for i, (j, k) in enumerate(indexes):
-        indexes_array[i, :] = j, k
-
-    # count number of coalescing pairs per node
-    nodes_weight = _pair_coalescence_counts(
-        indexes_array,
-        windows,
-        nodes_sample,
-        ts.edges_child.copy(),
-        ts.edges_parent.copy(),
-        ts.edges_left.copy(),
-        ts.edges_right.copy(),
-        ts.indexes_edge_insertion_order.copy(),
-        ts.indexes_edge_removal_order.copy(),
-        ts.sequence_length,
+    max_time = ts.nodes_time.max()
+    time_windows = np.logspace(*log_time_bounds, num_time_bins + 1)
+    pair_coalescence_counts = ts.pair_coalescence_counts(
+        sample_sets=sample_sets,
+        indexes=indexes,
+        windows=windows,
+        time_windows=time_windows,
+        pair_normalise=True,
+        span_normalise=True,
     )
-    
-    nodes_order = np.argsort(ts.nodes_time)
-    nodes_time = ts.nodes_time[nodes_order]
-    nodes_weight = nodes_weight[:, nodes_order, :]
+    #assert np.all(pair_coalescence_counts >= 0.0)
+    window_weights = window_weights * np.diff(windows)
+    window_weights /= np.sum(window_weights)
+    pair_coalescence_counts *= window_weights[:, np.newaxis, np.newaxis]
+    pair_coalescence_counts = np.sum(pair_coalescence_counts, axis=0)
+    # TODO: check
+    pair_coalescence_surv = np.hstack([
+        np.ones((pair_coalescence_counts.shape[0], 1)),
+        1 - np.cumsum(pair_coalescence_counts, axis=1),
+    ])
+    pair_coalescence_rates = (
+        np.log(pair_coalescence_surv[:, :-1]) - 
+        np.log(pair_coalescence_surv[:, 1:])
+    ) / np.diff(time_windows)[np.newaxis, :]
+    pair_coalescence_rates[~np.isfinite(pair_coalescence_rates)] = np.nan
+    # /TODO
+    epochs = np.tile(time_windows[:-1], (pair_coalescence_counts.shape[0], 1))
+    return pair_coalescence_rates.T, pair_coalescence_counts.T, epochs.T
 
-    # NB: global statistics must be weighted by the proportion of missing data
-    nodes_weight *= window_weights[:, np.newaxis, np.newaxis]
-    nodes_weight = nodes_weight.sum(axis=0)
-    assert nodes_weight.shape[0] == ts.num_nodes
-    assert nodes_weight.shape[1] == len(indexes)
-
-    # convert to coalescence rates
-    quantiles = np.linspace(0.0, 1.0, num_time_bins + 1)
-    coalrate = np.full((num_time_bins, nodes_weight.shape[1]), np.nan)
-    epochs = np.full((num_time_bins, nodes_weight.shape[1]), np.nan)
-    for i in range(nodes_weight.shape[1]):
-        inside_window = nodes_weight[:, i] > 0
-        assert np.sum(inside_window) > 0, "No data in window"
-        coalrate[:, i], epochs[:, i] = _rates_from_ecdf(
-            nodes_weight[inside_window, i], 
-            nodes_time[inside_window], 
-            quantiles,
-        )
-            
-    return coalrate, epochs
-
-
-# NB: use this for windowed statistics in a separate script (TODO)
-#def pair_coalescence_rates(ts, sample_sets, indexes, windows, num_time_bins=50):
-#    """
-#    Calculate pair coalescence rates within equally-spaced quantiles over a set
-#    of genomic windows. E.g. calculate the inverse of effective population size
-#    in N epochs wherein a proportion 1/N of pairs coalescece.
-#    """
-#
-#    nodes_sample = np.zeros((ts.num_nodes, len(sample_sets)))
-#    for i, s in enumerate(sample_sets):
-#        nodes_sample[s, i] = 1
-#
-#    indexes_array = np.zeros((len(indexes), 2), dtype=np.int32)
-#    for i, (j, k) in enumerate(indexes):
-#        indexes_array[i, :] = j, k
-#
-#    # count number of coalescing pairs per node
-#    nodes_weight = _pair_coalescence_counts(
-#        indexes_array,
-#        windows,
-#        nodes_sample,
-#        ts.edges_child.copy(),
-#        ts.edges_parent.copy(),
-#        ts.edges_left.copy(),
-#        ts.edges_right.copy(),
-#        ts.indexes_edge_insertion_order.copy(),
-#        ts.indexes_edge_removal_order.copy(),
-#        ts.sequence_length,
-#    )
-#    
-#    nodes_order = np.argsort(ts.nodes_time)
-#    nodes_time = ts.nodes_time[nodes_order]
-#    nodes_weight = nodes_weight[:, nodes_order, :]
-#
-#    # convert to coalescence rates
-#    quantiles = np.linspace(0.0, 1.0, num_time_bins + 1)
-#    coalrate = np.full((nodes_weight.shape[0], num_time_bins, nodes_weight.shape[2]), np.nan)
-#    epochs = np.full((nodes_weight.shape[0], num_time_bins, nodes_weight.shape[2]), np.nan)
-#    for w in range(nodes_weight.shape[0]):
-#        for i in range(nodes_weight.shape[2]):
-#            inside_window = nodes_weight[w, :, i] > 0
-#            if np.sum(inside_window) > 0:
-#                coalrate[w, :, i], epochs[w, :, i] = _rates_from_ecdf(
-#                    nodes_weight[w, inside_window, i], 
-#                    nodes_time[inside_window], 
-#                    quantiles,
-#                )
-#            
-#    return coalrate, epochs
 
 
 def simulation_test(seed, popsize, num_epochs):
@@ -302,9 +88,16 @@ ratemap = pickle.load(open(snakemake.input.ratemap, "rb"))
 sample_sets = [list(ts.samples())]
 indexes = [(0, 0)]
 windows = ratemap.position
-weights = ratemap.rate / np.sum(ratemap.rate)
-rates, breaks = weighted_pair_coalescence_rates(ts, sample_sets, indexes, windows, weights, num_intervals)
-output = {"rates" : np.squeeze(rates), "breaks" : np.squeeze(breaks)}
+weights = ratemap.rate # proportion missing times mutation rate
+rates, pdf, breaks = weighted_pair_coalescence_rates(
+    ts, sample_sets, indexes, windows, 
+    weights, num_time_bins=num_intervals,
+)
+output = {
+    "rates" : np.squeeze(rates), 
+    "pdf" : np.squeeze(pdf),
+    "breaks" : np.squeeze(breaks),
+}
 pickle.dump(output, open(snakemake.output.coalrate, "wb"))
 
 # stratified global pair coalescence rates (cross-coalescence)
@@ -317,15 +110,21 @@ if snakemake.params.stratify is not None:
     names = np.array(sorted(sample_sets.keys()))
     sample_sets = [sample_sets[x] for x in names]
     cross_rates = np.full((names.size, names.size, num_intervals), np.nan)
+    cross_pdf = np.full((names.size, names.size, num_intervals), np.nan)
     cross_breaks = np.full((names.size, names.size, num_intervals), np.nan)
     for i, _ in enumerate(names):
         indexes = [(i, j) for j in range(names.size)]
-        rates, breaks = \
-            weighted_pair_coalescence_rates(ts, sample_sets, indexes, windows, weights, num_intervals)
+        rates, pdf, breaks = \
+            weighted_pair_coalescence_rates(
+                ts, sample_sets, indexes, windows, 
+                weights, num_time_bins=num_intervals,
+            )
         cross_rates[i] = rates.T
+        cross_pdf[i] = pdf.T
         cross_breaks[i] = breaks.T
     output = {
         "rates" : cross_rates, 
+        "pdf" : cross_pdf,
         "breaks" : cross_breaks, 
         "names" : names,
     }
--- workflow/scripts/plot_coalescence_rates.py
+++ workflow/scripts/plot_coalescence_rates.py
@@ -11,7 +11,6 @@ Part of https://github.com/nspope/singer-snakemake.
 
 import sys
 import pickle
-import numba
 import msprime
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,81 +33,75 @@ num_burnin = snakemake.params.mcmc_burnin
 
 ratemap = pickle.load(open(snakemake.input.ratemap, "rb"))
 windows = ratemap.position
-quantiles = np.linspace(0, 1, num_intervals + 1)
-weights = -np.log(1 - np.diff(quantiles[:-1]) / (1 - quantiles[:-2]))
+
+# TODO: something off here why is mean lower than it should be
 
 # pair coalescence rates with all samples
-mean_rates = np.zeros(num_intervals)
-mean_breaks = np.zeros(num_intervals)
+reps = 0
+mean_pdf = np.zeros(num_intervals)
 for i, f in enumerate(snakemake.input.coalrate):
     inp = pickle.load(open(f, "rb"))
-    rates, breaks = np.squeeze(inp['rates']), np.squeeze(inp['breaks'])
+    pdf, breaks = np.squeeze(inp['pdf']), np.squeeze(inp['breaks'])
     if i >= num_burnin:
-        mean_rates += rates / (num_mcmc - num_burnin)
-        mean_breaks += breaks / (num_mcmc - num_burnin)
-        plt.step(breaks, rates, color='gray', alpha=0.1)
-mean_rates[:-1] = weights / np.diff(mean_breaks)
+        mean_pdf += pdf
+        reps += 1
+        plt.step(breaks, pdf, color='gray', alpha=0.1)
+mean_pdf /= reps
 
-plt.step(mean_breaks, mean_rates, color='black', linewidth=2)
+plt.step(breaks, mean_pdf, color='black', linewidth=1.5)
 plt.xlabel("Generations in past")
-plt.ylabel("Pair coalescence rate")
+plt.ylabel("Proportion coalescing pairs")
 plt.xscale("log")
-plt.yscale("log")
 plt.tight_layout()
-plt.savefig(snakemake.output.pair_coalescence_rates)
+plt.savefig(snakemake.output.pair_coalescence_pdf)
 plt.clf()
 
 # cross coalescence rates between strata
 if snakemake.params.stratify is not None:
     inp = pickle.load(open(snakemake.input.crossrate[0], "rb"))
-    names, rates, breaks = inp['names'], inp['rates'], inp['breaks']
+    names, pdf, breaks = inp['names'], inp['pdf'], inp['breaks']
 
     ncol = 3
     nrow = int(np.ceil(names.size / ncol))
     fig, axs = plt.subplots(nrow, ncol, figsize=(4 * ncol + 1, 4 * nrow), sharex=True, sharey=True, squeeze=False)
     cmap = plt.get_cmap("tab10", names.size)
 
-    mean_rates = rates / num_mcmc
-    mean_breaks = breaks / num_mcmc
+    reps = 0
+    mean_pdf = np.zeros_like(pdf)
     for i, f in enumerate(snakemake.input.crossrate[1:]):
-        if i + 1 >= num_burnin:
+        if i >= num_burnin:
             inp = pickle.load(open(f, "rb"))
-            rates, breaks = inp['rates'], inp['breaks']
-            mean_rates += rates / (num_mcmc - num_burnin)
-            mean_breaks += breaks / (num_mcmc - num_burnin)
+            pdf = inp['pdf']
+            mean_pdf += pdf
+            reps += 1
             for j, p in enumerate(names):
                 row, col = j // ncol, j % ncol 
                 for k, q in enumerate(names):
                     axs[row, col].step(
                         breaks[j, k], 
-                        rates[j, k], 
+                        pdf[j, k], 
                         color=cmap(k / names.size), 
                         alpha=0.02,
                     )
-
-    # all rates except terminal are one-to-one functions of interval width:
-    indices = np.arange(names.size)
-    for j, k in itertools.product(indices, indices):
-        mean_rates[j, k, :-1] = weights / np.diff(mean_breaks[j, k])
+    mean_pdf /= reps
 
     for j, p in enumerate(names):
         row, col = j // ncol, j % ncol 
         for k, q in enumerate(names):
             axs[row, col].step(
-                mean_breaks[j, k], 
-                mean_rates[j, k], 
+                breaks[j, k], 
+                mean_pdf[j, k], 
                 color=cmap(k / names.size), 
                 label=q, 
                 linewidth=2,
             )
         axs[row, col].set_title(f"With {p}", loc='center')
-        axs[row, col].set_yscale('log')
         axs[row, col].set_xscale('log')
     fig.supxlabel("Generations in the past")
-    fig.supylabel("Coalescence rate")
+    fig.supylabel("Proportion coalescencing pairs")
     legend = []
     for k, q in enumerate(names):
         legend.append(Line2D([0], [0], color=cmap(k / names.size), label=q, lw=2))
     fig.legend(handles=legend, loc='center left', bbox_to_anchor=(1, 0.5))
     fig.tight_layout()
-plt.savefig(snakemake.output.cross_coalescence_rates, bbox_inches="tight")
+plt.savefig(snakemake.output.cross_coalescence_pdf, bbox_inches="tight")
