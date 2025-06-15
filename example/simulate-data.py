Correct weighting for filtered sites in stats

example/simulate-data.py
@@ -73,7 +73,7 @@ remaining_sites = remaining_sites[remaining_sites > ts.sequence_length / 2]
 sitemask = np.sort(
     np.random.choice(
         remaining_sites.astype(np.int64) + 1,
-        size=int(0.1 * remaining_sites.size),
+        size=int(0.3 * remaining_sites.size),
         replace=False,
     )
 )

workflow/scripts/chunk_chromosomes.py
@@ -291,6 +291,9 @@ logfile.write(
 num_accessible[~filter_chunks] = 0
 num_retained[~filter_chunks] = 0
 num_filtered[~filter_chunks] = 0
+prop_inaccessible[~filter_chunks] = 1.0
+prop_filtered[~filter_chunks] = 0.0
+prop_segregating[~filter_chunks] = 0.0
 assert num_bases.sum() == bitmask.size
 assert num_accessible.sum() == np.sum(~bitmask)
 assert num_filtered.sum() == filtered_positions.size
@@ -374,12 +377,23 @@ for i in np.flatnonzero(filter_chunks):
     logfile.write(f"{tag()} Parameters for chunk {id} are {chunk_params}\n")
 
 
-# dump adjusted mutation rates and chunk coordinates
-ratemap = msprime.RateMap(
+# dump adjusted mutation rates, proportion accessible, and chunk coordinates
+adjusted_mu = msprime.RateMap(
     position=windows, 
     rate=np.array(adj_mu),
 )
-pickle.dump(ratemap, open(snakemake.output.ratemap, "wb"))
+pickle.dump(adjusted_mu, open(snakemake.output.adjusted_mu, "wb"))
+inaccessible = msprime.RateMap(
+    position=windows, 
+    rate=np.array(prop_inaccessible),
+)
+pickle.dump(inaccessible, open(snakemake.output.inaccessible, "wb"))
+filtered = msprime.RateMap(
+    position=windows, 
+    rate=np.array(prop_filtered),
+)
+pickle.dump(filtered, open(snakemake.output.filtered, "wb"))
+
 
 
 # dump filtered vcf

workflow/scripts/coalescence_rates.py
@@ -85,18 +85,24 @@ def simulation_test(seed, popsize, num_epochs):
 
 # --- implm --- #
 
+# TODO: check weights for statistics calculations
+# these are now wrong. we should only multiply each window
+# by the length of accessible sequence / sum of lengths
+
 num_intervals = snakemake.params.coalrate_epochs
+inaccessible = pickle.load(open(snakemake.input.inaccessible, "rb"))
+chunk_size = np.diff(inaccessible.position)
 ts = tszip.decompress(snakemake.input.trees)
-ratemap = pickle.load(open(snakemake.input.ratemap, "rb"))
+
 
 # global pair coalescence rates
 sample_sets = [list(ts.samples())]
 indexes = [(0, 0)]
-windows = ratemap.position
-weights = ratemap.rate # proportion missing times mutation rate
+windows = inaccessible.position
 rates, pdf, breaks = weighted_pair_coalescence_rates(
     ts, sample_sets, indexes, windows, 
-    weights, num_time_bins=num_intervals,
+    prop_accessible=1 - inaccessible.rate, 
+    num_time_bins=num_intervals,
 )
 output = {
     "rates" : np.squeeze(rates), 
@@ -104,6 +110,7 @@ output = {
     "breaks" : np.squeeze(breaks),
 }
 pickle.dump(output, open(snakemake.output.coalrate, "wb"))
+
 
 # stratified global pair coalescence rates (cross-coalescence)
 output = {}
@@ -122,7 +129,8 @@ if snakemake.params.stratify is not None:
         rates, pdf, breaks = \
             weighted_pair_coalescence_rates(
                 ts, sample_sets, indexes, windows, 
-                weights, num_time_bins=num_intervals,
+                prop_accessible=1 - inaccessible.rate, 
+                num_time_bins=num_intervals,
             )
         cross_rates[i] = rates
         cross_pdf[i] = pdf

workflow/scripts/tree_statistics.py
@@ -23,27 +23,34 @@ def tag():
 
 # --- implm --- #
 
-ratemap = pickle.load(open(snakemake.input.ratemap, "rb"))
-ts = tszip.decompress(snakemake.input.trees)
+inaccessible = pickle.load(open(snakemake.input.inaccessible, "rb"))
+filtered = pickle.load(open(snakemake.input.filtered, "rb"))
 mutation_rate = snakemake.params.mutation_rate
+chunk_size = np.diff(inaccessible.position)
+accessible_size = chunk_size * (1 - inaccessible.rate)
+ts = tszip.decompress(snakemake.input.trees)
 
 diversity = \
-    ts.diversity(mode='branch', windows=ratemap.position, span_normalise=True) * mutation_rate
-diversity[ratemap.rate == 0.0] = np.nan
+    ts.diversity(mode='branch', windows=inaccessible.position, span_normalise=True) * \
+    (1 - filtered.rate) * mutation_rate
+diversity[inaccessible.rate == 1.0] = np.nan
 
-tajima_d = ts.Tajimas_D(mode='branch', windows=ratemap.position)
-tajima_d[ratemap.rate == 0.0] = np.nan
+# NB: these are per-chunk; correctly calculating at the sequence level
+# would require calculating numerator and denominator separately, correcting,
+# and summing across chunks
+tajima_d = ts.Tajimas_D(mode='branch', windows=inaccessible.position)  # correction cancels within chunk
+tajima_d[inaccessible.rate == 1.0] = np.nan
 
-# NB: "global" statistics have to be weighted by missing data per block
+# NB: "global" statistics have to be weighted by accessible sequence length per chunk
+correction = (1 - filtered.rate) * accessible_size / np.sum(accessible_size)
+
 folded_afs = \
-    ts.allele_frequency_spectrum(mode='branch', windows=ratemap.position, span_normalise=True)
-folded_afs *= ratemap.rate[:, np.newaxis] / np.sum(ratemap.rate)
-folded_afs = np.sum(folded_afs, axis=0) * mutation_rate
+    ts.allele_frequency_spectrum(mode='branch', windows=inaccessible.position, span_normalise=True)
+folded_afs = np.sum(folded_afs * correction[:, np.newaxis] * mutation_rate, axis=0)
 
 unfolded_afs = \
-    ts.allele_frequency_spectrum(mode='branch', windows=ratemap.position, span_normalise=True, polarised=True) 
-unfolded_afs *= ratemap.rate[:, np.newaxis] / np.sum(ratemap.rate)
-unfolded_afs = np.sum(unfolded_afs, axis=0) * mutation_rate
+    ts.allele_frequency_spectrum(mode='branch', windows=inaccessible.position, span_normalise=True, polarised=True) 
+unfolded_afs = np.sum(unfolded_afs * correction[:, np.newaxis] * mutation_rate, axis=0)
 
 stats = {
     "diversity": diversity, 
@@ -52,6 +59,7 @@ stats = {
     "unfolded_afs": unfolded_afs,
 }
 pickle.dump(stats, open(snakemake.output.stats, "wb"))
+
 
 # TODO: clean this up
 # stratified summary stats
@@ -68,19 +76,18 @@ if snakemake.params.stratify is not None:
         ts.divergence(
             sample_sets=[s for s in sample_sets.values()], 
             indexes=list(itertools.combinations_with_replacement(range(len(strata)), 2)),
-            mode='branch', windows=ratemap.position, span_normalise=True,
-        ) * mutation_rate
-    strata_divergence[ratemap.rate == 0.0] = np.nan
+            mode='branch', windows=inaccessible.position, span_normalise=True,
+        ) * (1 - filtered.rate)[:, np.newaxis] * mutation_rate
+    strata_divergence[inaccessible.rate == 0.0] = np.nan
     strata_stats["divergence"] = strata_divergence
 
     strata_folded_afs = []
     for strata, sample_set in sample_sets.items():
         folded_afs = ts.allele_frequency_spectrum(
             sample_sets=[sample_set], mode='branch', 
-            windows=ratemap.position, span_normalise=True,
+            windows=inaccessible.position, span_normalise=True,
         )
-        folded_afs *= ratemap.rate[:, np.newaxis] / np.sum(ratemap.rate)
-        folded_afs = np.sum(folded_afs, axis=0) * mutation_rate
+        folded_afs = np.sum(folded_afs * correction[:, np.newaxis] * mutation_rate, axis=0)
         strata_folded_afs.append(folded_afs)
     strata_stats["folded_afs"] = strata_folded_afs
 
@@ -88,10 +95,9 @@ if snakemake.params.stratify is not None:
     for strata, sample_set in sample_sets.items():
         unfolded_afs = ts.allele_frequency_spectrum(
             sample_sets=[sample_set], mode='branch', polarised=True,
-            windows=ratemap.position, span_normalise=True,
+            windows=inaccessible.position, span_normalise=True,
         )
-        unfolded_afs *= ratemap.rate[:, np.newaxis] / np.sum(ratemap.rate)
-        unfolded_afs = np.sum(unfolded_afs, axis=0) * mutation_rate
+        unfolded_afs = np.sum(unfolded_afs * correction[:, np.newaxis] * mutation_rate, axis=0)
         strata_unfolded_afs.append(unfolded_afs)
     strata_stats["unfolded_afs"] = strata_unfolded_afs
 pickle.dump(strata_stats, open(snakemake.output.strata_stats, "wb"))